5993676d90e381114a1f2e0e7069c1a6623dfd18
Gabriel Salustriano <gabrielsalustriano5566@gmail.com>
2022-07-25 19:07:19 -0300

    Implementação: Menu da navbar / Fix: efeito degradê no menu

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -14,7 +14,17 @@ function Nav() {
   return (
     <div className={`nav-container ${show && 'nav-container-black'}`}>
         <img className='nav-logo' src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt='Netflix'></img>
+        <div className='nav-menu-container'>
+            <ul>
+              <li><a href='#'>Início</a></li>
+              <li><a href='#'>Séries</a></li>
+              <li><a href='#'>Filmes</a></li>
+              <li><a href='#'>Bombando</a></li>
+              <li><a href='#'>Minha lista</a></li>
+            </ul>
+        </div>
         <img className='nav-avatar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt='User'></img>
+        <div>▾</div>
     </div>
   )
 }

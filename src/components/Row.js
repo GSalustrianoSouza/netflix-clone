01afec84bc912cdb0a388262963e9281318dced6
Gabriel Salustriano <gabrielsalustriano5566@gmail.com>
2022-07-27 19:37:21 -0300

    Add: botões para passar os filmes / Issue: botão do lado direito não funciona

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,14 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactPlayer from 'react-player';
 import movieTrailer from 'movie-trailer'
 import { getMovies } from '../api';
 import './Row.css';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 const imageHost = "https://image.tmdb.org/t/p/original"
 
 function Row({title, path, isLarge }) {
     const [movies, setMovies] = React.useState([]);
 
+
+    // Inserir trailers ao clicar no filme
     const [trailerUrl, setTrailerUrl] = React.useState("");
     const handleOnClick = (movie) => {
         if(trailerUrl) {
@@ -24,6 +28,7 @@ function Row({title, path, isLarge }) {
         }     
     }
 
+    // Puxar filmes da API
     const fetchMovies = async (_path) => {
         try {
             const data = await getMovies(_path);
@@ -38,10 +43,36 @@ function Row({title, path, isLarge }) {
       fetchMovies(path);
     }, [path])
 
+    // Scroll movies function
+    const [scrollX, setScrollX] = useState(-400);
+    // Função de scrollar para direita
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if (x > 0) {
+            x = 0;
+        }
+        setScrollX(x);
+    }
+    // Função de scrollar para esquerda
+    const handleRightArrow = () => {
+        let x = scrollX - Math.round(window.innerWidth / 2);
+        let listW = movies.results.length * 150;
+        if((window.innerWidth - listW) > x) {
+            x = (window.innerWidth - listW) - 60;
+        }
+        setScrollX(x);
+    }
+
     return (
         <div className="row-container">
             <h2 className="row-header">{title}</h2>
-            <div className="row-cards">
+            <div className='movieRow-left' onClick={handleLeftArrow}>
+                <NavigateBeforeIcon style={{fontSize: 50}} />
+            </div>
+            <div className='movieRow-right' onClick={handleRightArrow}>
+                <NavigateNextIcon style={{fontSize: 50}} />
+            </div>
+            <div className="row-cards" style={{marginLeft: scrollX}}>
                 {movies?.map(movie => {
                     return (
                         <img 
